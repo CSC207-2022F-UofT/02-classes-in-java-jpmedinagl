4ced5f16bd64ec890da1e097664145a35b5949e5
jpmedinagl <jpmedinagl@gmail.com>
2023-05-14 19:35:24 -0400

    modifying files

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -16,11 +16,11 @@ public class Silly implements Comparable<Silly>{
      * 1. Java has variables known as "static" variables.
      * These are variables that exist in every instance of a class,
      * and which have the same value across all instances.
-     *
+
      * Below we have created a static variable (signified by the keyword
      * static) named my_static. The countStatic() method makes use of this
      * static variable.
-     *
+
      * (Relevant reading: 2.1. Static variables)
      */
     public static int my_static = 0;
@@ -41,15 +41,15 @@ public class Silly implements Comparable<Silly>{
         return to_return;
     }
 
-    /**
-     * 2. Java allows for us to overload methods: this means we can have
-     *    multiple methods of the same name, but which take different
-     *    parameters (both in terms of numbers and type).
-     *
-     *    Below we have created two different constructors and have left
-     *    a third for you to write.
-     *
-     *    (Relevant reading: 2.2. Overloading)
+    /*
+      2. Java allows for us to overload methods: this means we can have
+         multiple methods of the same name, but which take different
+         parameters (both in terms of numbers and type).
+
+         Below we have created two different constructors and have left
+         a third for you to write.
+
+         (Relevant reading: 2.2. Overloading)
      */
 
     /**
@@ -72,27 +72,36 @@ public class Silly implements Comparable<Silly>{
         this.name = "Silly #" + number;
     }
 
-    /**
-     * TODO (Task 1): Create another constructor that takes in TWO parameters,
-     *       both of which are strings.
-     *       Afterwards, set this.name to the concatenation of both
-     *       strings (e.g. this.name = [first string] + [second string]).
-     *       Make sure you document this method!
+    /*
+      TODO (Task 1): Create another constructor that takes in TWO parameters,
+            both of which are strings.
+            Afterwards, set this.name to the concatenation of both
+            strings (e.g. this.name = [first string] + [second string]).
+            Make sure you document this method!
      */
 
-
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two strings as arguments.
+     *
+     * @param s1 the first string for the instance's name.
+     * @param s2 the second string for the instance's name.
+     **/
+    public Silly(String s1, String s2) {
+        this.name = s1 + s2;
+    }
 
 
 
     public static void main(String[] args) {
-        /**
-         * Task 1 (continued): The below demonstrates how each of the Silly
-         *                     constructors are called.
-         *
-         * This is simply provided as an example for you to see whether your
-         * code is working correctly, and to see how the different calls appear
-         *
-         * To run this, Task 3 is also required to be completed.
+        /*
+          Task 1 (continued): The below demonstrates how each of the Silly
+                              constructors are called.
+
+          This is simply provided as an example for you to see whether your
+          code is working correctly, and to see how the different calls appear
+
+          To run this, Task 3 is also required to be completed.
          */
         Silly first_version = new Silly("single str name");
         Silly second_version = new Silly(5);
@@ -106,17 +115,17 @@ public class Silly implements Comparable<Silly>{
         Silly x = new Silly("something");
         Silly y = new Silly("something else");
 
-        /**
-         * TODO (Task 2): Below are a bunch of calls to countStatic using
-         *                two different instances of Silly.
-         *                Fill out the expected_values array with the
-         *                values that countStatic will return at each call.
+        /*
+          TODO (Task 2): Below are a bunch of calls to countStatic using
+                         two different instances of Silly.
+                         Fill out the expected_values array with the
+                         values that countStatic will return at each call.
          */
         x.countStatic();
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -124,9 +133,9 @@ public class Silly implements Comparable<Silly>{
     /**
      * 3. Whenever we try to build a string with an Object, the toString()
      *    method is called. This is equivalent to the __str__ method in Python.
-     *
+
      *    (Relevant reading: 2.3. toString)
-     *
+
      * Return the String representation of this Silly object (i.e. its name).
      *
      * @return the name of this Silly.
@@ -134,13 +143,14 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
      * 4. We can also override the equals() method, which is the equivalent of
      *    Python's __eq__; however, unlike Python, this is called when we use
      *    .equals() rather than == (which checks for ID equality in Java).
-     *
+
      *    (Relevant reading: 2.4. Equality)
      *
      * @param o   the object to compare to
@@ -148,10 +158,10 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public boolean equals(Object o) {
-        /**
-         * TODO (Task 4): Complete the body of this method!
-         *                We've started it by checking the type of o for you.
-         *                You just need to return true if the names are equal.
+        /*
+          TODO (Task 4): Complete the body of this method!
+                         We've started it by checking the type of o for you.
+                         You just need to return true if the names are equal.
          */
         if (!(o instanceof Silly)){
             return false;
@@ -161,6 +171,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return this.name.equals((other.name));
     }
 
     /**
@@ -171,11 +182,11 @@ public class Silly implements Comparable<Silly>{
      *    ii) override the compareTo method. This takes another object
      *        (of the same type if we specify the classname when we write
      *        'implements Comparable'), and returns an integer.
-     *
+
      *    (Relevant reading: 2.5. Comparing objects)
-     *
+
      * Compares this object with other for order.
-     *
+
      * Returns a negative integer, zero, or a positive integer as this
      * object is less than, equal to, or greater than the specified object.
      *
@@ -185,15 +196,16 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public int compareTo(Silly other) {
-        /**
-         * TODO (Task 5): Implement the body of this method.
-         *                A positive number should be returned if this.name
-         *                is longer than other.name, a negative number if
-         *                other.name is longer than this.name, and 0 if
-         *                the lengths are equal.
-         *                You can get the length of a string by using the
-         *                .length() method.
+        /*
+          TODO (Task 5): Implement the body of this method.
+                         A positive number should be returned if this.name
+                         is longer than other.name, a negative number if
+                         other.name is longer than this.name, and 0 if
+                         the lengths are equal.
+                         You can get the length of a string by using the
+                         .length() method.
          */
+        return this.name.length() - other.name.length();
     }
 
     /*
